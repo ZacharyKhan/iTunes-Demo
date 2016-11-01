e5527a64c7fe68d86e3e5c97aab5f816ef9534a9
Zach <zachary.khan3@gmail.com>
2016-11-01 00:10:55 -0700

    added extension

---
+++ iTunes-Demo/iTunes-Demo/Entensions.swift
@@ -0,0 +1,25 @@
+//
+//  Entensions.swift
+//  iTunes-Demo
+//
+//  Created by Zachary Khan on 10/31/16.
+//  Copyright © 2016 ZacharyKhan. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addConstraintsWithFormat(_ format: String, views: UIView...) {
+        
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+    
+}
